b0d026095d0786dad52ac6ef0544b9c43f047b94
Arasu Ramanan <arasuaudi@gmail.com>
2026-01-20 20:04:11 +0530

    third commit

--- 2/script.js
+++ 2/script.js
@@ -1,8 +1,8 @@
 let todos = ["Learn JS", "Do Assignment","Take Break"];
 
 function output(arasu){
-for(i = 0; i <todos.length; i++){
-    console.log(todos[i]);
+for(i = 0; i <arasu.length; i++){
+    console.log(arasu[i]);
 }
 }
 output(todos)
